8712f36d2323ac28f1deaae095b9a2a74e4caa49
elderlyman <ryaneavey@gmail.com>
2023-12-11 12:02:46 -0800

    removed env file, changed build

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           TEST FRICKIN' TEST CHANGE!!!!
+          REMOVED ENV FILE totally
         </p>
         <a
           className="App-link"
